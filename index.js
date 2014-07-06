f4ee74b689351498a89f8f77ee8d21f68b806ad1
Blaine Bublitz <blaine@iceddev.com>
2014-07-06 12:33:17 -0700

    add rxjs support, needs tests - ref #3

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var eos = require('end-of-stream');
 var tick = require('next-tick');
 var once = require('once');
 
+function noop(){}
+
 function asyncDone(fn, cb){
   var done = once(cb);
 
@@ -25,13 +27,21 @@ function asyncDone(fn, cb){
     var result = domainBoundFn(done);
 
     if(result && typeof result.on === 'function'){
+      // assume node stream
       d.add(result);
       eos(result, { error: false }, onSuccess);
       return;
     }
 
     if(result && typeof result.then === 'function'){
+      // assume promise
       result.then(onSuccess, onError);
+      return;
+    }
+
+    if(result && typeof result.subscribe === 'function'){
+      // assume RxJS observable
+      result.subscribe(noop, onError, onSuccess);
       return;
     }
   }
