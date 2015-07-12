0bd126240ac9a4b07c9a572e03697afbe552e883
Blaine Bublitz <blaine@iceddev.com>
2015-07-12 13:45:29 -0700

    support multiple parameters to the final callback - closes #23

---
+++ test/arguments.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = require('code').expect;
+
+var asyncDone = require('../');
+
+function twoArg(cb){
+  cb(null, 1, 2);
+}
+
+describe('arguments', function(){
+
+  it('passes all arguments to the completion callback', function(done){
+    asyncDone(twoArg, function(err, arg1, arg2){
+      expect(arg1).to.equal(1);
+      expect(arg2).to.equal(2);
+      done(err);
+    });
+  });
+});
